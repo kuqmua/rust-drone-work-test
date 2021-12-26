bd934625f90f96bf35ba323cc51bde755d97dd46
kuqmua <serezhashmakov@outlook.com>
2021-12-26 16:40:25 +0300

    form component css

--- yew-client/src/form.rs
+++ yew-client/src/form.rs
@@ -31,203 +31,209 @@ impl Component for Form {
         let link = ctx.link();
         html! {
           <div
-            class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6"
+            class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12"
             style="
-              padding: 8px;
-              flex-grow: 0;
-              max-width: 100%;
-              flex-basis: 100%;
-              margin: 0;
-              box-sizing: border-box;
-              display: block;
+                -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.87);
+    font-size: 0.875rem;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1.43;
+    letter-spacing: 0.01071em;
+    margin: 0;
+    box-sizing: border-box;
+    flex-grow: 0;
+    max-width: 100%;
+    flex-basis: 100%;
+    padding: 8px;
             "
           >
             <div
               class="MuiFormControl-root MuiTextField-root MuiFormControl-fullWidth"
               style="
-                width: 100%;
-                border: 0;
-                margin: 0;
-                display: inline-flex;
-                padding: 0;
-                position: relative;
-                min-width: 0;
-                flex-direction: column;
-                vertical-align: top;
-                box-sizing: inherit;
+                  -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.87);
+    font-size: 0.875rem;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1.43;
+    letter-spacing: 0.01071em;
+    box-sizing: inherit;
+    border: 0;
+    margin: 0;
+    display: inline-flex;
+    padding: 0;
+    position: relative;
+    min-width: 0;
+    flex-direction: column;
+    vertical-align: top;
+    width: 100%;
               "
             >
               <label
                 class="MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined Mui-required Mui-required"
-                style="
-                  z-index: 1;
-                  transform: translate(14px, 20px) scale(1);
-                  pointer-events: none;
-                  transition: color 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms,transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;
-                  top: 0;
-                  left: 0;
-                  position: absolute;
-                  display: block;
-                  transform-origin: top left;
-                  color: rgba(0, 0, 0, 0.54);
-                  padding: 0;
-                  font-size: 1rem;
-                  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                  font-weight: 400;
-                  line-height: 1;
-                  letter-spacing: 0.00938em;
-                  box-sizing: inherit;
-                  cursor: default;
-                "
                 data-shrink="false"
-                for="firstName"
-                id="firstName-label"
+                for="email"
+                id="email-label"
+                style="
+                    -webkit-font-smoothing: antialiased;
+    box-sizing: inherit;
+    color: rgba(0, 0, 0, 0.54);
+    padding: 0;
+    font-size: 1rem;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1;
+    letter-spacing: 0.00938em;
+    display: block;
+    transform-origin: top left;
+    top: 0;
+    left: 0;
+    position: absolute;
+    transition: color 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms,transform 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;
+    z-index: 1;
+    transform: translate(14px, 20px) scale(1);
+    pointer-events: none;
+                "
               >
-                {"First Name"}
+                {"Email Address"}
                 <span
                   aria-hidden="true"
                   class="MuiFormLabel-asterisk MuiInputLabel-asterisk"
                   style="
-                    box-sizing: inherit;
-                    color: rgba(0, 0, 0, 0.54);
-                    padding: 0;
-                    font-size: 1rem;
-                    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                    font-weight: 400;
-                    line-height: 1;
-                    letter-spacing: 0.00938em;
-                    cursor: default;
+                      -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.54);
+    font-size: 1rem;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1;
+    letter-spacing: 0.00938em;
+    pointer-events: none;
+    box-sizing: inherit;
                   "
                 >
-                  {"*"}
+                {" *"}
                 </span>
               </label>
               <div
                 class="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-fullWidth MuiInputBase-formControl"
                 style="
-                  position: relative;
-                  border-radius: 4px;
-                  width: 100%;
-                  color: rgba(0, 0, 0, 0.87);
-                  cursor: text;
-                  display: inline-flex;
-                  position: relative;
-                  font-size: 1rem;
-                  box-sizing: border-box;
-                  align-items: center;
-                  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                  font-weight: 400;
-                  line-height: 1.1876em;
-                  letter-spacing: 0.00938em;
+                    -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.87);
+    cursor: text;
+    display: inline-flex;
+    font-size: 1rem;
+    box-sizing: border-box;
+    align-items: center;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1.1876em;
+    letter-spacing: 0.00938em;
+    width: 100%;
+    position: relative;
+    border-radius: 4px;
                 "
               >
                 <input
                   aria-invalid="false"
-                  autocomplete="fname"
-                  id="firstName"
-                  name="firstName"
-                  // required=""
+                  autocomplete="email"
+                  id="email"
+                  name="email"
+                //   required=""
                   type="text"
                   class="MuiInputBase-input MuiOutlinedInput-input"
-                  // value=""
+                  value=""
                   style="
-                    box-shadow: none;
-                    padding: 18.5px 14px;
-                    font: inherit;
-                    color: currentColor;
-                    width: 100%;
-                    border: 0;
-                    height: 1.1876em;
-                    margin: 0;
-                    display: block;
-                    padding: 6px 0 7px;
-                    min-width: 0;
-                    background: none;
-                    box-sizing: content-box;
-                    animation-name: mui-auto-fill-cancel;
-                    letter-spacing: inherit;
-                    animation-duration: 10ms;
-                    -webkit-tap-highlight-color: transparent;
-                    -webkit-writing-mode: horizontal-tb !important;
-                    text-rendering: auto;
-                    word-spacing: normal;
-                    text-transform: none;
-                    text-indent: 0px;
-                    text-shadow: none;
-                    text-align: start;
-                    appearance: auto;
-                    -webkit-rtl-ordering: logical;
-                    cursor: text;
+                      -webkit-font-smoothing: antialiased;
+    font: inherit;
+    color: currentColor;
+    width: 100%;
+    border: 0;
+    height: 1.1876em;
+    margin: 0;
+    display: block;
+    min-width: 0;
+    background: none;
+    box-sizing: content-box;
+    animation-name: mui-auto-fill-cancel;
+    letter-spacing: inherit;
+    animation-duration: 10ms;
+    -webkit-tap-highlight-color: transparent;
+    padding: 18.5px 14px;
+    box-shadow: none;
                   "
                 />
-                <fieldset aria-hidden="true"
+                <fieldset
+                  aria-hidden="true"
                   class="PrivateNotchedOutline-root-5 MuiOutlinedInput-notchedOutline"
                   style="
-                    border-color: rgba(0, 0, 0, 0.23);
-                    top: -5px;
-                    left: 0;
-                    right: 0;
-                    bottom: 0;
-                    margin: 0;
-                    padding: 0 8px;
-                    overflow: hidden;
-                    position: absolute;
-                    border-style: solid;
-                    border-width: 1px;
-                    border-radius: inherit;
-                    pointer-events: none;
-                    box-sizing: inherit;
-                    display: block;
-                    margin-inline-start: 2px;
-                    margin-inline-end: 2px;
-                    padding-block-start: 0.35em;
-                    padding-inline-start: 0.75em;
-                    padding-inline-end: 0.75em;
-                    padding-block-end: 0.625em;
-                    min-inline-size: min-content;
-                    border-image: initial;
-                    color: rgba(0, 0, 0, 0.87);
-                    cursor: text;
-                    display: inline-flex;
-                    position: relative;
-                    font-size: 1rem;
-                    box-sizing: border-box;
-                    align-items: center;
-                    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                    font-weight: 400;
-                    line-height: 1.1876em;
-                    letter-spacing: 0.00938em;
+                      -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.87);
+    cursor: text;
+    font-size: 1rem;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1.1876em;
+    letter-spacing: 0.00938em;
+    box-sizing: inherit;
+    top: -5px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: 0;
+    padding: 0 8px;
+    overflow: hidden;
+    position: absolute;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: inherit;
+    pointer-events: none;
+    border-color: rgba(0, 0, 0, 0.23);
                   "
                 >
                   <legend
                     class="PrivateNotchedOutline-legendLabelled-7"
                     style="
-                      width: auto;
-                      height: 11px;
-                      display: block;
-                      padding: 0;
-                      font-size: 0.75em;
-                      max-width: 0.01px;
-                      text-align: left;
-                      transition: max-width 50ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;
-                      visibility: hidden;
-                      box-sizing: inherit;
-                      padding-inline-start: 2px;
-                      padding-inline-end: 2px;
-                      border-width: initial;
-                      border-style: none;
-                      border-color: initial;
-                      border-image: initial;
+                        -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.87);
+    cursor: text;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1.1876em;
+    letter-spacing: 0.00938em;
+    pointer-events: none;
+    box-sizing: inherit;
+    width: auto;
+    height: 11px;
+    display: block;
+    padding: 0;
+    font-size: 0.75em;
+    max-width: 0.01px;
+    text-align: left;
+    transition: max-width 50ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;
+    visibility: hidden;
                     "
                   >
                     <span
                       style="
-                        display: inline-block;
-                        padding-left: 5px;
-                        padding-right: 5px;
+                          -webkit-font-smoothing: antialiased;
+    color: rgba(0, 0, 0, 0.87);
+    cursor: text;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1.1876em;
+    letter-spacing: 0.00938em;
+    pointer-events: none;
+    font-size: 0.75em;
+    text-align: left;
+    visibility: hidden;
+    box-sizing: inherit;
+    display: inline-block;
+    padding-left: 5px;
+    padding-right: 5px;
                       "
                     >
-                      {"First Name&nbsp;*"}
+                      {"Email Address&nbsp;*"}
                     </span>
                   </legend>
                 </fieldset>
