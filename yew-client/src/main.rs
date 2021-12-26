27713a9d6c3ef5efd8aafc1f8793193e2144da46
kuqmua <serezhashmakov@outlook.com>
2021-12-26 15:57:57 +0300

    SvgIconWrapper component

--- yew-client/src/main.rs
+++ yew-client/src/main.rs
@@ -1,8 +1,10 @@
 pub mod form;
 pub mod input_button;
+pub mod svg_icon_wrapper;
 
 use crate::form::Form;
 use crate::input_button::InputButton;
+use crate::svg_icon_wrapper::SvgIconWrapper;
 
 use yew::prelude::*;
 
@@ -74,51 +76,7 @@ impl Component for Model {
                   flex-direction: column;
                 "
               >
-                <div
-                  class="MuiAvatar-root MuiAvatar-circular makeStyles-avatar-2 MuiAvatar-colorDefault"
-                  style="
-                    margin: 8px;
-                    background-color: #19857b;
-                    color: #fff;
-                    width: 40px;
-                    height: 40px;
-                    display: flex;
-                    overflow: hidden;
-                    position: relative;
-                    font-size: 1.25rem;
-                    align-items: center;
-                    flex-shrink: 0;
-                    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                    line-height: 1;
-                    user-select: none;
-                    border-radius: 50%;
-                    justify-content: center;
-                  "
-                >
-                  <svg
-                    style="
-                      width: 75%;
-                      height: 75%;
-                      fill: currentColor;
-                      width: 1em;
-                      height: 1em;
-                      display: inline-block;
-                      font-size: 1.5rem;
-                      transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-                      flex-shrink: 0;
-                      user-select: none;
-                    "
-                    class="MuiSvgIcon-root MuiAvatar-fallback"
-                    focusable="false"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
-                    >
-                    </path>
-                  </svg>
-                </div>
+                <SvgIconWrapper />
                 <h1
                   class="MuiTypography-root MuiTypography-h5"
                   style="
